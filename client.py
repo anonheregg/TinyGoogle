ce008bfdeda019720f0266739af7af4679c05451
Guhan <90622371+guhanarul@users.noreply.github.com>
2023-06-01 13:10:29 +0530

    Update client.py

--- client.py
+++ client.py
@@ -1,5 +1,6 @@
 import socket
 import struct
+import os
 
 def send_image(image_path, server_ip, server_port):
     # Create a TCP socket
@@ -21,6 +22,7 @@ def send_image(image_path, server_ip, server_port):
         # Send the image data to the server
         client_socket.sendall(image_data)
         print("Image sent successfully")
+        os.remove(f"detected/{item}")
     except Exception as e:
         print("Error:", e)
     finally:
@@ -29,9 +31,23 @@ def send_image(image_path, server_ip, server_port):
         print("Connection closed")
 
 # Specify the image file path, server IP, and server port
-image_path = "path_to_image.jpg"
-server_ip = "127.0.0.1"  # Replace with the server IP address
-server_port = 5000  # Replace with the server port
+'''try:
+    for _,_,files in os.walk("detected"):
+        for file1 in files:
+            if(str(file1)[-3:]=="jpg"):
+                image_path = f"detected/{file1}"
+                server_ip = "127.0.0.1"  # Replace with the server IP address
+                server_port = 5000  # Replace with the server port
 
-# Call the send_image function
-send_image(image_path, server_ip, server_port)
+                # Call the send_image function
+                send_image(image_path, server_ip, server_port)
+                os.remove(f"detected/{file1}")'''
+while(0<1):
+    folder_contents = os.listdir("detected")
+    for item in folder_contents:
+        if(str(item)[-4:]=="jpeg" or str(item)[-3:]=="txt"):
+            image_path=f"detected/{item}"
+            server_ip="127.0.0.1"
+            server_port=5500
+            send_image(image_path,server_ip,server_port)
+        
