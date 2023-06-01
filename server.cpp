a4b718de016a9f4a695b3a6bc86cbae8e966c833
Guhan <90622371+guhanarul@users.noreply.github.com>
2023-06-01 13:09:44 +0530

    Update server.cpp

--- server.cpp
+++ server.cpp
@@ -1,95 +1,55 @@
-#include <bits/stdc++.h>
-#include <fstream>
-#include <sstream>
-#include <cstring>
-#include <arpa/inet.h>
-#include <sys/socket.h>
-#include <unistd.h>
-using namespace std;
+import socket
+import struct
 
-void receive_image(const std::string& save_path, const std::string& server_ip, int server_port) {
-    int server_socket = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_socket == -1) {
-        std::cerr << "Failed to create server socket." << std::endl;
-        return;
-    }
+def receive_image(save_path, server_ip, server_port):
+    
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    struct sockaddr_in server_address{};
-    server_address.sin_family = AF_INET;
-    server_address.sin_addr.s_addr = inet_addr(server_ip.c_str());
-    server_address.sin_port = htons(server_port);
+    try:
+        
+        server_socket.bind((server_ip, server_port))
 
-    if (bind(server_socket, (struct sockaddr*)&server_address, sizeof(server_address)) < 0) {
-        std::cerr << "Failed to bind server socket." << std::endl;
-        close(server_socket);
-        return;
-    }
+        
+        server_socket.listen(1)
+        print("Server is listening on {}:{}".format(server_ip, server_port))
 
-    if (listen(server_socket, 1) < 0) {
-        std::cerr << "Failed to listen on server socket." << std::endl;
-        close(server_socket);
-        return;
-    }
+        
+        client_socket, client_address = server_socket.accept()
+        print("Connected to client:", client_address)
 
-    std::cout << "Server is listening on " << server_ip << ":" << server_port << std::endl;
+        
+        size_data = client_socket.recv(4)
+        image_size = struct.unpack("!I", size_data)[0]
 
-    struct sockaddr_in client_address{};
-    socklen_t client_address_len = sizeof(client_address);
-    int client_socket = accept(server_socket, (struct sockaddr*)&client_address, &client_address_len);
-    if (client_socket < 0) {
-        std::cerr << "Failed to accept client connection." << std::endl;
-        close(server_socket);
-        return;
-    }
+        
+        image_data = b""
+        remaining_bytes = image_size
+        while remaining_bytes > 0:
+            chunk = client_socket.recv(min(4096, remaining_bytes))
+            if not chunk:
+                break
+            image_data += chunk
+            remaining_bytes -= len(chunk)
+        if(image_data[1]!=108):
+            with open(save_path,"wb") as file:
+                file.write(image_data)
+        else:
+            with open(save_path[:-4]+"txt","wb") as file:
+                file.write(image_data)
+        print("Image received and saved successfully")
+    except Exception as e:
+        print("Error:", e)
+    finally:
+        
+        server_socket.close()
+        print("Server socket closed")
+i=1
+while(0<1):
+    save_path = f"received/received_image{i}.jpeg"
+    #server_ip = "192.168.92.246" 
+    server_ip="127.0.0.1" 
+    server_port = 5500 
 
-    std::cout << "Connected to client: " << inet_ntoa(client_address.sin_addr) << std::endl;
 
-    uint32_t image_size;
-    if (recv(client_socket, &image_size, sizeof(image_size), 0) < 0) {
-        std::cerr << "Failed to receive image size." << std::endl;
-        close(client_socket);
-        close(server_socket);
-        return;
-    }
-    image_size = ntohl(image_size);
-
-    std::cout << "Image size: " << image_size << " bytes" << std::endl;
-
-    std::vector<uint8_t> image_data(image_size);
-    uint32_t remaining_bytes = image_size;
-    uint32_t offset = 0;
-    while (remaining_bytes > 0) {
-        uint32_t chunk_size = std::min(4096u, remaining_bytes);
-        uint32_t received_bytes = recv(client_socket, &image_data[offset], chunk_size, 0);
-        if (received_bytes <= 0) {
-            break;
-        }
-        remaining_bytes -= received_bytes;
-        offset += received_bytes;
-    }
-
-    std::ofstream output_file(save_path, std::ios::binary);
-    if (!output_file) {
-        std::cerr << "Failed to open output file: " << save_path << std::endl;
-        close(client_socket);
-        close(server_socket);
-        return;
-    }
-    output_file.write(reinterpret_cast<const char*>(image_data.data()), image_data.size());
-    output_file.close();
-
-    std::cout << "Image received and saved successfully" << std::endl;
-
-    close(client_socket);
-    close(server_socket);
-}
-
-int main() {
-    std::string save_path = "received_image.jpg";
-    std::string server_ip = "172.16.5.224";
-    int server_port = 5000;
-
-    receive_image(save_path, server_ip, server_port);
-
-    return 0;
-}
+    receive_image(save_path, server_ip, server_port)
+    i+=1
